7f4d445468a12916482e8ac8d3ab8fe22dca8f78
dkkim001 <xeexxee2@gmail.com>
2025-08-02 06:31:15 +0000

    뉴스 호출방식 변경

--- src/features/News/services/NewsService.js
+++ src/features/News/services/NewsService.js
@@ -1,13 +1,5 @@
-// newsService.js
+// src/services/newsService.js
+
 const SPREADSHEET_ID = '1e8HHRx4U6OSiITnLaeHyfjQFWuq-g0KHkPbMrnUaY_Y';
-const SHEET_NAME = 'HR'; // 스프레드시트 시트 이름
-
-const PROXY_URL = `https://sheet.best/api/sheets/${SPREADSHEET_ID}/${SHEET_NAME}`;
-
-export const fetchNewsData = async () => {
-  const response = await fetch(PROXY_URL);
-  if (!response.ok) {
-    throw new Error('네트워크 응답이 올바르지 않습니다.');
-  }
-  return response.json();
-};
+const SHEET_NAME = 'HR'; // 변경된 시트 이름 사용
+const PROXY_URL = `https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/gviz/tq?tqx=out:json&sheet=${SHEET_NAME}`;
